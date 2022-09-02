4fc49f885babfafff9c8f140fe506d4649631b33
Nicolas Loyau <nicolas.loyau@epita.fr>
2022-09-02 14:28:06 +0200

    fix(discord): set content type on webhook push and fix user pseudo

--- utils/discord.ts
+++ utils/discord.ts
@@ -3,6 +3,10 @@ export const sendApplyNotification = async (name, id) => {
 
   await fetch(process.env.NEXT_PUBLIC_DISCORD_APPLY_HOOK_URL, {
     method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       content: `Nouvelle candidature de ${name} ! @everyone\nhttps://secretdesanciens.fr/apply/${id}`,
       username: "Secret des Anciens",
@@ -17,12 +21,16 @@ export const sendLogNotification = async (user, name, state) => {
 
   let content = "";
 
-  if (state === "delete") content = `**[DELETE][${date}]** ${user} deleted ${name}'s apply`;
-  else if (state === "edit") content = `**[EDIT][${date}]** ${user} edited ${name}'s apply`;
-  else content = `**[STATUS][${date}]** ${user} changed ${name}'s apply status to ${state}`;
+  if (state === "delete") content = `**[DELETE][${date}]** ${user.pseudo} deleted ${name}'s apply`;
+  else if (state === "edit") content = `**[EDIT][${date}]** ${user.pseudo} edited ${name}'s apply`;
+  else content = `**[STATUS][${date}]** ${user} changed ${name.pseudo}'s apply status to ${state}`;
 
   await fetch(process.env.NEXT_PUBLIC_DISCORD_LOG_HOOK_URL, {
     method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       content: content,
       username: "Secret des Anciens",
